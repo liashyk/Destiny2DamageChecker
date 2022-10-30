55915282ca021fe40a405bbdbcd784de82a473ce
liashyk <ilya9may@gmail.com>
2022-10-30 15:17:16 +0200

    Add project files.

---
+++ Destiny2DamageChecker/Models/Archetype.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Destiny2DataLibrary.Models
+{
+    public class Archetype
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int RoundsPerMinute { get; set; }
+        public int BulletDamage { get; set; }
+        public ICollection<Perk> Perks { get; set; }
+    }
+}
---
+++ Destiny2DamageChecker/Models/Perk.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Destiny2DataLibrary.Models
+{
+    public class Perk
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public string? Summary { get; set; }
+        public int ActivationStepsAmount { get; set; }
+        public int MyProperty { get; set; }
+
+        public ICollection<Archetype> Archetypes { get; set; }
+        
+    }
+}
